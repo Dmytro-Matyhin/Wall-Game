c28851f03fb39d3715cd536c0f41b04fc09857c2
Dmytro <dima.mattock@gmail.com>
2019-12-09 22:46:08 +0200

    Remove redundant components

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -16,24 +16,7 @@ export default class App extends React.Component {
     };
   }
 
-  handleButtonClick = (fieldName, x, y, newNumber) => {
-    const fieldClone = cloneField(this.state[fieldName]);
-    fieldClone[y][x] = newNumber;
-
-    this.setState({
-      [fieldName]: fieldClone,
-    });
-  };
-
   render = () => {
-    console.log(this.state.fieldPlayer1);
-    console.log(cloneField(this.state.fieldPlayer1));
 
-    return (
-      <div>
-        <Field field={this.state.fieldPlayer1} fieldName='fieldPlayer1' player='player-1' onButtonClick={this.handleButtonClick}/>
-        <Field field={this.state.fieldPlayer2} fieldName='fieldPlayer2' player='player-2' onButtonClick={this.handleButtonClick} />
-      </div>
-    );
   };
 }
